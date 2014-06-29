7c288f3367968f59612a690b3fa16a74e7211f77
Andrew S. Brown <andrew@dontfidget.com>
2014-06-29 10:47:10 -0700

    move code climate loading to very start of spec helper as indicated by code climate docs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 require 'active_support'
 require 'action_view'
 require 'action_controller'
 require 'rails/engine'
 require 'rspec/rails'
 require 'pry'
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
 
 module Rails
   def self.application
